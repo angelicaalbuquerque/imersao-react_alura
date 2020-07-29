33b4d0d4abc8eaf78bdf5afb5add1df775b7a2c0
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-07-28 23:13:17 -0300

    Remoção de console.log

--- angelicaflix/src/components/Menu/components/ButtonLink/index.js
+++ angelicaflix/src/components/Menu/components/ButtonLink/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 function ButtonLink(props) {
-  //props => { className: "O que alguém passar", href: "/" }
-  // as {} são utilizadas para colocar o JS dentro do HTML, que está dentro do JS. Usado sempre para valores dinâmicos
-
   console.log(props);
 
   return (
