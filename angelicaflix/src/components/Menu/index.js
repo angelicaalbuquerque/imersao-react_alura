bec047d4e19b5c07162d1654e2519e290f9edc22
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-07-29 22:54:37 -0300

    Componente Button

--- angelicaflix/src/components/Menu/index.js
+++ angelicaflix/src/components/Menu/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../../assets/img/Logo.png";
 import "./menu.css";
-import ButtonLink from "./components/ButtonLink";
+import Button from "../Button";
 
 function Menu() {
   return (
@@ -10,9 +10,9 @@ function Menu() {
         <img className="Logo" src={Logo} alt="Angelicaflix logo" />
       </a>
 
-      <ButtonLink className="ButtonLink" href="/">
+      <Button as="a" className="ButtonLink" href="/">
         Novo vídeo
-      </ButtonLink>
+      </Button>
     </nav>
   );
 }
